fcff69082d4f82e27b76d5b70d0a6dc275cf5d05
sd0116 <diegormtz9@gmail.com>
2024-11-27 20:02:57 -0600

    PWA

--- To_Do_List/Lista/static/js/service-worker.js
+++ To_Do_List/Lista/static/js/service-worker.js
@@ -1,20 +1,24 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('todo-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/static/css/styles.css',
-                '/static/js/app.js',
-                '/static/manifest.json'
-            ]);
-        })
-    );
+const CACHE_NAME = 'task-manager-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/styles.css',
+  '/static/scripts.js',
+  '/static/icons/icon-192x192.png',
+  '/static/icons/icon-512x512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
